085cbd463a6a444d1fa3adc5d4fe0b3a07c1dcbe
more-ginger <francesca1morini@gmail.com>
2021-08-02 14:45:11 +0200

    load font with @font-face

--- advanced/main.js
+++ advanced/main.js
@@ -95,6 +95,8 @@ var Tooltip = {
 
 // add click events to all svg elements that have an entry in data file
 function load() {
+    let draggableContainer = x('#draggable-container')
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
     // variable holds the viewport width
     var currentViewportWidth = detectedViewportWidth();
     
@@ -177,6 +179,47 @@ function load() {
 			 reset();
 		 }
 	}
+
+    // The current position of mouse
+    // let xCoord = 0;
+    // let yCoord = 0;
+
+    // // Query the element
+    // const ele = document.getElementById('draggable-container');
+
+    // // Handle the mousedown event
+    // // that's triggered when user drags the element
+    // const mouseDownHandler = function (e) {
+    //     // Get the current mouse position
+    //     xCoord = e.clientX;
+    //     yCoord = e.clientY;
+
+    //     // Attach the listeners to `document`
+    //     document.addEventListener('mousemove', mouseMoveHandler);
+    //     document.addEventListener('mouseup', mouseUpHandler);
+    // };
+
+    // const mouseMoveHandler = function (e) {
+    //     // How far the mouse has been moved
+    //     const dx = e.clientX - xCoord;
+    //     const dy = e.clientY - yCoord;
+
+    //     // Set the position of element
+    //     ele.style.top = `${ele.offsetTop + dy}px`;
+    //     ele.style.left = `${ele.offsetLeft + dx}px`;
+
+    //     // Reassign the position of mouse
+    //     xCoord = e.clientX;
+    //     yCoord = e.clientY;
+    // };
+
+    // const mouseUpHandler = function () {
+    //     // Remove the handlers of `mousemove` and `mouseup`
+    //     document.removeEventListener('mousemove', mouseMoveHandler);
+    //     document.removeEventListener('mouseup', mouseUpHandler);
+    // };
+
+    // ele.addEventListener('mousedown', mouseDownHandler);
 }
 
 ////////////////////////
