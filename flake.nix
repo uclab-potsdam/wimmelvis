4d92641d211c7565e8997b3c83987e9e30521703
Kerstin Humm <kerstin@erictapen.name>
2022-12-09 13:56:46 +0100

    nixpkgs-fmt flake.nix

--- flake.nix
+++ flake.nix
@@ -3,35 +3,38 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-    nodejs = pkgs."nodejs-18_x";
-  in {
-
-    packages.x86_64-linux.website = let
-      nodeEnv = import ./study/node-env.nix {
-        inherit (pkgs) stdenv lib python2 runCommand writeTextFile writeShellScript;
-        inherit pkgs nodejs;
-        libtool = if pkgs.stdenv.isDarwin then pkgs.darwin.cctools else null;
-      };
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      nodejs = pkgs."nodejs-18_x";
     in
-    (import ./study/node-packages.nix {
-      inherit (pkgs) fetchurl nix-gitignore stdenv lib fetchgit;
-      inherit nodeEnv;
-    }).package.override {
-      postInstall = ''
-        npm run build
-      '';
+    {
+
+      packages.x86_64-linux.website =
+        let
+          nodeEnv = import ./study/node-env.nix {
+            inherit (pkgs) stdenv lib python2 runCommand writeTextFile writeShellScript;
+            inherit pkgs nodejs;
+            libtool = if pkgs.stdenv.isDarwin then pkgs.darwin.cctools else null;
+          };
+        in
+        (import ./study/node-packages.nix {
+          inherit (pkgs) fetchurl nix-gitignore stdenv lib fetchgit;
+          inherit nodeEnv;
+        }).package.override {
+          postInstall = ''
+            npm run build
+          '';
+        };
+
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.website;
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          node2nix
+        ];
+      };
+
     };
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.website;
-
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        nodejs
-        node2nix
-      ];
-    };
-
-  };
 }
