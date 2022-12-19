ddc36ceae1929dbc018720f4301deaf5310f4640
more-ginger <francesca1morini@gmail.com>
2022-12-19 16:50:59 +0100

    close #25

--- study/linear/build-linear-items.py
+++ study/linear/build-linear-items.py
@@ -8,23 +8,29 @@ import json
 
 html_snippet = '''\
     <div id="{item_id}" class="{classes}">
-      <img src="../common/assets/items/{item_id}_w_symbol.png">
-      <h3>{heading}</h3>
-      <p>
-        {text}
-      </p>
+      <div class="element-container">
+        <div class="img-container">
+          <img src="../common/assets/items/{item_id}_w_symbol.png">
+        </div>
+        <div class="description">
+        <h3>{heading}</h3>
+        <p>
+          {text}
+        </p>
+        </div>
+      </div>
     </div>
 '''
 
-even = False
+uneven = False
 
 with open("./common/assets/waste_data.json") as json_file:
-  items = json.load(json_file)
-  for item_id, item in items.items():
-    print(html_snippet.format(
-      item_id = item_id,
-      classes = "linear-item" + " " + ("even" if even else "uneven"),
-      heading = item["name_EN"],
-      text = item["material_info_EN"]
-    ))
-    even = not even
+    items = json.load(json_file)
+    for item_id, item in items.items():
+        print(html_snippet.format(
+            item_id=item_id,
+            classes="linear-item" + " " + ("uneven" if uneven else "even"),
+            heading=item["name_EN"],
+            text=item["material_info_EN"]
+        ))
+        uneven = not uneven
