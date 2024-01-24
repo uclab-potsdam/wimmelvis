61190c294e7d19b8c726093d969329abb9074a83
Kerstin Humm <kerstin@erictapen.name>
2024-01-24 19:09:14 +0100

    use flake-utils to become independent of architecture

--- flake.nix
+++ flake.nix
@@ -1,40 +1,48 @@
 {
   description = "A very basic flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      nodejs = pkgs."nodejs-18_x";
-    in
-    {
-
-      packages.x86_64-linux.website =
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem
+      [ "x86_64-linux" "aarch64-linux" ]
+      (system:
         let
-          nodeEnv = import ./study/node-env.nix {
-            inherit (pkgs) stdenv lib python2 runCommand writeTextFile writeShellScript;
-            inherit pkgs nodejs;
-            libtool = if pkgs.stdenv.isDarwin then pkgs.darwin.cctools else null;
-          };
+          pkgs = nixpkgs.legacyPackages.${system};
+          nodejs = pkgs."nodejs-18_x";
         in
-        (import ./study/node-packages.nix {
-          inherit (pkgs) fetchurl nix-gitignore stdenv lib fetchgit;
-          inherit nodeEnv;
-        }).package.override {
-          postInstall = ''
-            npm run build
-          '';
-        };
+        {
 
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.website;
+          packages = {
 
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nodejs
-          node2nix
-        ];
-      };
+            default = self.packages.${system}.website;
+            website =
+              let
+                nodeEnv = import ./study/node-env.nix {
+                  inherit (pkgs) stdenv lib python2 runCommand writeTextFile writeShellScript;
+                  inherit pkgs nodejs;
+                  libtool = if pkgs.stdenv.isDarwin then pkgs.darwin.cctools else null;
+                };
+              in
+              (import ./study/node-packages.nix {
+                inherit (pkgs) fetchurl nix-gitignore stdenv lib fetchgit;
+                inherit nodeEnv;
+              }).package.override {
+                postInstall = ''
+                  npm run build
+                '';
+              };
+          };
 
-    };
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs
+              node2nix
+            ];
+          };
+
+        });
 }
